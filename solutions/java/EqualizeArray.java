5c1e59e9d44fe3b723405005be1528daa03a9dd9
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-11 20:15:51 +0000

    java solution ok

--- solutions/java/EqualizeArray.java
+++ solutions/java/EqualizeArray.java
@@ -1,3 +1,30 @@
 public class EqualizeArray{
-	
+	static int equalizeArray(int[] arr) {
+        Arrays.sort(arr);
+        int lastEl = 0; // as the lower constraint is 1 we can set the first to 0
+        int biggerCount = 0;
+        int currentCount = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i == 0){
+                lastEl = arr[i];
+                currentCount = 1;
+                continue;
+            }
+            
+            if(arr[i] == lastEl){
+                currentCount++;
+                if(i == arr.length - 1 && biggerCount < currentCount){
+                    biggerCount = currentCount;
+                }
+            }else{
+                if(biggerCount < currentCount){
+                    biggerCount = currentCount;
+                }
+                lastEl = arr[i];
+                currentCount = 1;
+            }
+        }
+        return arr.length - biggerCount;
+    }
 }
